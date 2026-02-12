9f25159221aee094706c9a3024134f69c2df3791
Linijarae <alexis.dupin@ynov.com>
2026-02-12 21:21:28 +0100

    Add Chp15 and chp20 folders + main.py chp 15

---
+++ Chapitre 15/main.py
@@ -0,0 +1,20 @@
+import sys
+import platform
+import os
+
+def verifier_environnement():
+    
+    print("--- Rapport d'environnement Python ---")
+    print(f"Version Python : {sys.version.split()[0]}")
+    print(f"Système OS     : {platform.system()} {platform.release()}")
+    print(f"Exécutable     : {sys.executable}")
+    try:
+        import pip
+        print(f"Version Pip    : {pip.__version__}")
+    except ImportError:
+        print("Version Pip    : Non installé (Attention : nécessaire pour installer des bibliothèques)")
+    print("-" * 38)
+    print("Votre environnement est prêt à exécuter des scripts.")
+
+if __name__ == "__main__":
+    verifier_environnement()
